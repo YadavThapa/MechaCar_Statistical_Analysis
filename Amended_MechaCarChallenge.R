7f635ffd74cbcd72bdb33f9a9dac2f106d5da5d3
YadavThapa <hemjaliyadav@yahoo.com>
2020-10-29 12:45:22 -0400

    Update Amended_MechaCarChallenge.R

--- Amended_MechaCarChallenge.R
+++ Amended_MechaCarChallenge.R
@@ -28,10 +28,11 @@ summary(mylr)
 ## Deliverable-2 Create Visualizations for the Trip Analysis 
 ## part a : summarising data without using group_by function
 #############################################################
-
+## Import and read the File
 suspension_coil_df = read.csv(file.choose())
 str(suspension_coil_df)
 
+## Summarising the data without group by Function 
 sum_suspension_df <- suspension_coil_df %>% summarize(
   mean= mean(PSI),
   median= median(PSI),
@@ -41,10 +42,18 @@ sum_suspension_df <- suspension_coil_df %>% summarize(
 
 sum_suspension_df
 
-
+##############################################################
+## Import and read the file
 df = read.csv(file.choose())
+
+## Converting Char variables in to factor variable using as.factor in R 
 df$Manufacturing_Lot = as.factor(df$Manufacturing_Lot)
+
+## Summarising the variable in R with group_by function
 final<- df%>%
-  group_by(Manufacturing_Lot)%>%
-  summarise(mean(PSI),median(PSI),sd(PSI))
+  group_by(Manufacturing_Lot)%>% summarise(
+    mean= mean(PSI),
+    median = median(PSI),
+    staddev = sd(PSI),
+  variance= var(PSI))
 View(final)
